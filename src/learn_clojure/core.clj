e3484a390d54c4c311000e1d63311f5177f5da4a
Aseem Chopra <aseemchopra@protonmail.com>
2023-05-13 14:34:24 +0530

    update: foo2

--- src/learn_clojure/core.clj
+++ src/learn_clojure/core.clj
@@ -3,5 +3,9 @@
 (def foo 
   (let [l "light"] (println (str "Let there be " l "!"))))
 
+(def foo2
+(let [d "darkness"] (println (str "Let there also be " d "!"))))
+
 (defn -main []
-  foo)
+  foo
+  foo2)
